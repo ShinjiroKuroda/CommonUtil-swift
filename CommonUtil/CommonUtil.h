9751258299d7df151bb27b4c3cebb527aab7d3d2
Shinjiro Kuroda <kuroman4@gmail.com>
2019-06-24 12:05:38 +0900

    Initial Commit

---
+++ CommonUtil/CommonUtil.h
@@ -0,0 +1,19 @@
+//
+//  CommonUtil.h
+//  CommonUtil
+//
+//  Created by Shinjiro Kuroda on 2019/06/24.
+//  Copyright © 2019 AppTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonUtil.
+FOUNDATION_EXPORT double CommonUtilVersionNumber;
+
+//! Project version string for CommonUtil.
+FOUNDATION_EXPORT const unsigned char CommonUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonUtil/PublicHeader.h>
+
+
