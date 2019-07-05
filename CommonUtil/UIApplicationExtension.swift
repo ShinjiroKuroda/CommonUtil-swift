213ca00dd9824634891a8253b66ce6c6adbb4ebe
Shinjiro Kuroda <kuroman4@gmail.com>
2019-07-05 14:50:57 +0900

    TopViewControllerの追加。

---
+++ CommonUtil/UIApplicationExtension.swift
@@ -0,0 +1,28 @@
+//
+//  UIApplication+Extension.swift
+//  veve
+//
+//  Created by 黒田 真次郎 on 2018/05/12.
+//  Copyright © 2018年 黒田 真次郎. All rights reserved.
+//
+
+import UIKit
+
+/** usage
+ UIApplication.shared.topViewController
+ **/
+
+public extension UIApplication {
+    public var topViewController: UIViewController? {
+        guard var topViewController = UIApplication.shared.keyWindow?.rootViewController else { return nil }
+        
+        while let presentedViewController = topViewController.presentedViewController {
+            topViewController = presentedViewController
+        }
+        return topViewController
+    }
+    
+    public var topNavigationController: UINavigationController? {
+        return topViewController as? UINavigationController
+    }
+}
